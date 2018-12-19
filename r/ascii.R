825377fc6a9f993cc796fc45092caa22c6861863
Javier Luraschi <javierluraschi@hotmail.com>
2018-12-19 12:16:07 -0800

    support for notes

--- r/ascii.R
+++ r/ascii.R
@@ -33,3 +33,22 @@ ascii_add_image_captions <- function(line) {
 
   line
 }
+
+ascii_add_notes <- function(lines) {
+  all_lines <- paste(lines, collapse = "\n")
+
+  while (grepl("\n\\*Note:\\*.*\n\n", all_lines)) {
+    r <- regexec("\n\\*Note:\\*.*\n\n", all_lines)
+
+    start_idx <- r[[1]][1]
+    start <- substr(all_lines, 1, start_idx)
+    remaining <- substr(all_lines, start_idx, nchar(all_lines))
+    end_idx <- regexec("\n\n", remaining)[[1]][1]
+    replacement <- substr(all_lines, start_idx, start_idx + end_idx - 1)
+    end <- substr(remaining, end_idx, nchar(remaining))
+
+    all_lines <- paste0(start, "[NOTE]\n===", gsub("\\*Note:\\* +", "", replacement), "===\n", end)
+  }
+
+  strsplit(all_lines, "\n")[[1]]
+}
