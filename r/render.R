dc144f6788b81d05f4fffdbd9b039a5eba6ae5ce
Javier Luraschi <javierluraschi@hotmail.com>
2018-12-21 11:22:56 -0800

    tweak to image rendering

--- r/render.R
+++ r/render.R
@@ -1,9 +1,8 @@
 render_nomnoml <- function(code, png, caption, styles = "") {
-  diagram <- nomnoml::nomnoml(paste0(styles, "\n", code), png = png)
-
   if (identical(Sys.getenv("ASCIITEXT_RENDERING"), "TRUE")) {
+    nomnoml::nomnoml(paste0(styles, "\n", code), png = png)
     knitr::asis_output(paste0("![", caption, "](", png, ")"))
   } else {
-    diagram
+    nomnoml::nomnoml(paste0(styles, "\n", code))
   }
 }
