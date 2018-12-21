5254c5bb893f94e38010e3955a0891d0fbb6e1a9
Javier Luraschi <javierluraschi@hotmail.com>
2018-12-21 11:28:57 -0800

    only rebuild images when needed

--- r/render.R
+++ r/render.R
@@ -1,6 +1,7 @@
 render_nomnoml <- function(code, png, caption, styles = "") {
   if (identical(Sys.getenv("ASCIITEXT_RENDERING"), "TRUE")) {
-    nomnoml::nomnoml(paste0(styles, "\n", code), png = png)
+    if (!file.exists(png))
+      nomnoml::nomnoml(paste0(styles, "\n", code), png = png)
     knitr::asis_output(paste0("![", caption, "](", png, ")"))
   } else {
     nomnoml::nomnoml(paste0(styles, "\n", code))
