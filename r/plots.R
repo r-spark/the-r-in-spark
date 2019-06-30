a39a529ff6d627ca8d43f6440bd4c661220982a9
Javier Luraschi <javierluraschi@hotmail.com>
2019-06-30 09:13:24 -0700

    all: avoid going past marging and appendix update

--- r/plots.R
+++ r/plots.R
@@ -3,31 +3,28 @@ plot_style <- function() {
 
   ggplot2::theme_classic() +
   ggplot2::theme(
-    plot.title = ggplot2::element_text(family = font,
-                                       size=14,
-                                       color = "#222222"),
-    plot.subtitle = ggplot2::element_text(family=font,
-                                          size=12,
-                                          color = "#666666"),
+    plot.title = ggplot2::element_text(
+      family = font, size=14, color = "#222222"),
+    plot.subtitle = ggplot2::element_text(
+      family=font, size=12, color = "#666666"),
 
     legend.position = "right",
     legend.background = ggplot2::element_blank(),
     legend.title = ggplot2::element_blank(),
     legend.key = ggplot2::element_blank(),
-    legend.text = ggplot2::element_text(family=font,
-                                        size=14,
-                                        color="#222222"),
+    legend.text = ggplot2::element_text(
+      family=font, size=14, color="#222222"),
 
-    axis.title.y = ggplot2::element_text(margin = ggplot2::margin(t = 0, r = 8, b = 0, l = 0),
-                                size = 14,
-                                color="#666666"),
-    axis.title.x = ggplot2::element_text(margin = ggplot2::margin(t = -2, r = 0, b = 0, l = 0),
-                                size = 14,
-                                color = "#666666"),
-    axis.text = ggplot2::element_text(family=font,
-                                      size=14,
-                                      color="#222222"),
-    axis.text.x = ggplot2::element_text(margin = ggplot2::margin(5, b = 10)),
+    axis.title.y = ggplot2::element_text(
+      margin = ggplot2::margin(t = 0, r = 8, b = 0, l = 0),
+      size = 14, color="#666666"),
+    axis.title.x = ggplot2::element_text(
+      margin = ggplot2::margin(t = -2, r = 0, b = 0, l = 0),
+      size = 14, color = "#666666"),
+    axis.text = ggplot2::element_text(
+      family=font, size=14, color="#222222"),
+    axis.text.x = ggplot2::element_text(
+      margin = ggplot2::margin(5, b = 10)),
     axis.ticks = ggplot2::element_blank(),
     axis.line = ggplot2::element_blank(),
 
