b10ff9a5af9e4fc5ec3e3a94cc60049ee979b5a6
Javier Luraschi <javierluraschi@hotmail.com>
2019-07-09 15:36:41 -0700

    tests: copy scripts to unblock rendering

--- tests/testthat/helper-initialize.R
+++ tests/testthat/helper-initialize.R
@@ -4,6 +4,7 @@ if (dir.exists("sources")) unlink("output", recursive = TRUE)
 if (!dir.exists("sources")) dir.create("sources")
 
 file.copy(dir("../../", pattern = "*.Rmd", full.names = TRUE), "sources/", recursive = TRUE)
+file.copy("../../r/", "sources/", recursive = TRUE)
 file.copy("../../data/", "sources/", recursive = TRUE)
 file.copy("../../images/", "sources/", recursive = TRUE)
 
