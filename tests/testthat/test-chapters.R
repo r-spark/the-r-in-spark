591a9c97f41a3590c7787a5e11693a1b0e345315
Javier Luraschi <javierluraschi@hotmail.com>
2019-07-09 19:14:17 -0700

    tests: knit without callr

--- tests/testthat/test-chapters.R
+++ tests/testthat/test-chapters.R
@@ -11,7 +11,7 @@ test_that("can knit starting", {
 })
 
 test_that("can knit analysis", {
-  callr::r(rmarkdown::render("sources/analysis.Rmd", output_dir = "output"))
+  rmarkdown::render("sources/analysis.Rmd", output_dir = "output")
   expect_true(file.exists("output/analysis.html"))
 })
 
