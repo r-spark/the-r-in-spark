b5402939faed44b7ae005bb94860b8219221bc8b
Javier Luraschi <javierluraschi@hotmail.com>
2019-09-11 13:05:16 -0700

    support extracting code chunks

--- r/exercises.R
+++ r/exercises.R
@@ -4,12 +4,9 @@ exercises_chapters <- list(
   "modeling",
   "pipelines",
   "data",
-  "tuning",
   "extensions",
   "distributed-r",
-  "streaming",
-  "contributing",
-  "appendix"
+  "streaming"
 )
 
 build_exercises <- function() {
@@ -17,16 +14,46 @@ build_exercises <- function() {
 
   file.copy(
     dir(".", pattern = paste(chapters, collapse = "|"), full.names = TRUE),
-    "excersises/"
+    "excersises/",
+    overwrite = TRUE
   )
 
   for (file in dir("excersises", full.names = TRUE)) {
+    reg_comment <- "^#+ [A-Z][a-z]+.*$"
+    reg_rcode <- "^```\\{r .*"
+    reg_code <- "^```$"
+
     content <- readLines(file)
-    matches <- which(grepl("^#+ [A-Z][a-z]+.*$", content))
+    matches <- which(grepl(paste(reg_comment, reg_rcode, reg_code, sep = "|"), content))
+    match_endings <- which(grepl("^```$", content))
 
     new_content <- c()
-    for (match in matches) {
-      new_content <- c(new_content, content[match])
+    match_idx <- 1
+    while (match_idx <= length(matches)) {
+      match <- matches[match_idx]
+
+      comment <- grepl(reg_comment, content[match])
+      rcode <- grepl(reg_rcode, content[match])
+
+      if (comment) {
+        new_content <- c(new_content, "", content[match])
+
+        # Skip nested rmarkdown in analysis chapter
+        if (grepl("## Visualize", content[match])) match_idx <- length(matches) + 1
+
+        match_idx <- match_idx + 1
+      }
+      else {
+        ending_idx <- which(match_endings > match)
+        if (length(ending_idx) == 0) warning("Can't find closing statement in ", file, "#", matches[match_idx], " for: ", content[match])
+
+        if (rcode) {
+          new_content <- c(new_content, "", content[match:match_endings[ending_idx[1]]])
+        }
+
+        match_next <- which(matches > match_endings[ending_idx[1]])
+        match_idx <- if (length(match_next) == 0 ) length(matches) + 1 else match_next[1]
+      }
     }
 
     writeLines(new_content, file)
