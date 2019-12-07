c6764917426c9b220f1d361ae0caa77960c8694c
Javier Luraschi <javierluraschi@hotmail.com>
2019-12-06 16:02:37 -0800

    fix image size for website

--- r/render.R
+++ r/render.R
@@ -1,5 +1,5 @@
 render_nomnoml <- function(code, png, caption = NULL, styles = "") {
-  defaults <- "#lineWidth: 2\n"
+  defaults <- "#lineWidth: 1\n"
   if (identical(Sys.getenv("ASCIITEXT_RENDERING"), "TRUE")) {
     nomnoml::nomnoml(paste0(defaults, "\n", styles, "\n", code), png = png)
     png_resized <- resize_image_if_needed(png)
